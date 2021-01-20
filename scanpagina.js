df7cf6920cadae6fc1fa46bca3477f41e4cdae42
gapisabelle <57439367+gapisabelle@users.noreply.github.com>
2021-01-20 22:03:32 +0100

    20-1 markers zijn werkend en data kan naar een andere pagina worden verzonden.

--- scanpagina.js
+++ scanpagina.js
@@ -1,2 +1,23 @@
-var favoritemovie = "Shrek";
-sessionStorage.setItem("favoriteMovie", favoritemovie);
+
+
+
+AFRAME.registerComponent('registerevents', {
+
+
+  init: function () {
+    var marker = this.el;
+
+    marker.addEventListener('markerFound', function() {
+      var markerId = marker.id;
+      console.log('markerFound', markerId);
+      var favoritemovie = "Shrek";
+      sessionStorage.setItem("favoriteMovie", favoritemovie);
+      window.location.href = "smoelenboek.html";
+    });
+
+    marker.addEventListener('markerLost', function() {
+      var markerId = marker.id;
+      console.log('markerLost', markerId);
+    });
+  }
+});
