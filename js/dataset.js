5c571d3b3640e30dca0a875ca5cfaf17c341dc00
Davino Rosaria <doubled.rosaria@gmail.com>
2021-01-25 16:59:57 +0100

    removed dataset file

--- js/dataset.js
+++
@@ -1,7 +0,0 @@
-var data = 0;
-
-function blenderModelUpdate() {
-    data = sessionStorage.setItem('data', 1);
-    console.log(sessionStorage.getItem('data'));
-    document.location.href = '/plein.html';
-}
