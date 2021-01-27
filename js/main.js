540e44bbfd8506ac5ca72dd02032398ff086a6f6
Davino Rosaria <doubled.rosaria@gmail.com>
2021-01-27 20:38:28 +0100

    mini changes
    rare ding

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 window.onload = function () {
+    var marker1 = sessionStorage.getItem("marker1");
+    var marker2 = sessionStorage.getItem("marker2");
+    var marker3 = sessionStorage.getItem("marker3");
+    goedeMarker1 = "Giraf";
+    goedeMarker2 = "Pinguin";
+    goedeMarker3 = "Olifant";
+
     const places = document.getElementsByClassName("js--place");
     const camera = document.getElementById("js--camera");
     const scene = document.querySelector('a-scene');
@@ -9,163 +16,182 @@ window.onload = function () {
 
     // dynamically create a-entity with according values.
     const giraffe = () => {
+        if (goedeMarker1 == marker1) {
 
-        const model = document.createElement("a-entity");
-        model.setAttribute("position", {
-            x: -5,
-            y: -2.2,
-            z: -5
-        });
-        model.setAttribute("rotation", {
-            x: 0,
-            y: 110,
-            z: 0
-        });
-        model.setAttribute("animation-mixer", "loop:repeat");
-        model.setAttribute("scale", {
-            x: 0.4,
-            y: 0.4,
-            z: 0.4
-        });
 
-        // Chose the right gltf 3D model based on recieved data.
-        switch ("5") {
+            const model = document.createElement("a-entity");
+            model.setAttribute("position", {
+                x: -5,
+                y: -2.2,
+                z: -5
+            });
+            model.setAttribute("rotation", {
+                x: 0,
+                y: 110,
+                z: 0
+            });
+            model.setAttribute("animation-mixer", "loop:repeat");
+            model.setAttribute("scale", {
+                x: 0.4,
+                y: 0.4,
+                z: 0.4
+            });
 
-            case "1":
-                model.setAttribute("gltf-model", "#rafrafwalking")
-                break;
+            // Chose the right gltf 3D model based on recieved data.
+            switch (receivedPData) {
 
-            case "2":
-                model.setAttribute("gltf-model", "#rafrafwalking_phoed")
-                break;
+                case "1":
+                    model.setAttribute("gltf-model", "#rafrafwalking")
+                    break;
 
-            case "3":
-                model.setAttribute("gltf-model", "#rafrafwalking_bhoed")
-                break;
+                case "2":
+                    model.setAttribute("gltf-model", "#rafrafwalking_phoed")
+                    break;
 
-            case "4":
-                model.setAttribute("gltf-model", "#rafrafwalking_stropdas")
-                break;
+                case "3":
+                    model.setAttribute("gltf-model", "#rafrafwalking_bhoed")
+                    break;
 
-            case "5":
-                model.setAttribute("gltf-model", "#rafrafwalking_phoed_stropdas")
-                break;
+                case "4":
+                    model.setAttribute("gltf-model", "#rafrafwalking_stropdas")
+                    break;
 
-            case "6":
-                model.setAttribute("gltf-model", "#rafrafwalking_bhoed_stropdas")
-                break;
+                case "5":
+                    model.setAttribute("gltf-model", "#rafrafwalking_phoed_stropdas")
+                    break;
 
+                case "6":
+                    model.setAttribute("gltf-model", "#rafrafwalking_bhoed_stropdas")
+                    break;
+
+            }
+
+            scene.appendChild(model);
+
+            console.log(receivedGData);
+        } else {
+            console.log("geen giraffe")
         }
-
-        scene.appendChild(model);
     }
-    console.log(receivedGData);
 
     const olifant = () => {
-        const model = document.createElement("a-entity");
-        model.setAttribute("position", {
-            x: 6,
-            y: -2.1,
-            z: -6
-        });
-        model.setAttribute("rotation", {
-            x: 0,
-            y: -20,
-            z: 0
-        });
-        model.setAttribute("animation-mixer", "loop:repeat");
-        model.setAttribute("scale", {
-            x: 0.7,
-            y: 0.7,
-            z: 0.7
-        });
+        if (goedeMarker3 == marker3) {
 
-        switch ("5") {
+            const model = document.createElement("a-entity");
+            model.setAttribute("position", {
+                x: 6,
+                y: -2.1,
+                z: -6
+            });
+            model.setAttribute("rotation", {
+                x: 0,
+                y: -20,
+                z: 0
+            });
+            model.setAttribute("animation-mixer", "loop:repeat");
+            model.setAttribute("scale", {
+                x: 0.7,
+                y: 0.7,
+                z: 0.7
+            });
 
-            case "1":
-                model.setAttribute("gltf-model", "#olifant")
-                break;
+            switch ("5") {
 
-            case "2":
-                model.setAttribute("gltf-model", "#olifant_phoed")
-                break;
+                case "1":
+                    model.setAttribute("gltf-model", "#olifant")
+                    break;
 
-            case "3":
-                model.setAttribute("gltf-model", "#olifant_bhoed")
-                break;
+                case "2":
+                    model.setAttribute("gltf-model", "#olifant_phoed")
+                    break;
 
-            case "4":
-                model.setAttribute("gltf-model", "#olifant_stropdas")
-                break;
+                case "3":
+                    model.setAttribute("gltf-model", "#olifant_bhoed")
+                    break;
 
-            case "5":
-                model.setAttribute("gltf-model", "#olifant_phoed_stropdas")
-                break;
+                case "4":
+                    model.setAttribute("gltf-model", "#olifant_stropdas")
+                    break;
 
-            case "6":
-                model.setAttribute("gltf-model", "#olifant_bhoed_stropdas")
-                break;
+                case "5":
+                    model.setAttribute("gltf-model", "#olifant_phoed_stropdas")
+                    break;
 
+                case "6":
+                    model.setAttribute("gltf-model", "#olifant_bhoed_stropdas")
+                    break;
+
+            }
+
+            scene.appendChild(model);
+
+            console.log(receivedOData);
+
+        } else {
+            console.log("geen olifant")
         }
-
-        scene.appendChild(model);
     }
-    console.log(receivedOData);
 
     const pinguin = () => {
-        const model = document.createElement("a-entity");
-        model.setAttribute("position", {
-            x: 0,
-            y: -1.90,
-            z: 3
-        });
-        model.setAttribute("rotation", {
-            x: 0,
-            y: 180,
-            z: 0
-        });
-        model.setAttribute("animation-mixer", "loop:repeat");
-        model.setAttribute("scale", {
-            x: .3,
-            y: .3,
-            z: .3
-        });
+        if (goedeMarker2 == marker2) {
 
-        switch ("5") {
+            const model = document.createElement("a-entity");
+            model.setAttribute("position", {
+                x: 0,
+                y: -1.90,
+                z: 3
+            });
+            model.setAttribute("rotation", {
+                x: 0,
+                y: 180,
+                z: 0
+            });
+            model.setAttribute("animation-mixer", "loop:repeat");
+            model.setAttribute("scale", {
+                x: .3,
+                y: .3,
+                z: .3
+            });
 
-            case "1":
-                model.setAttribute("gltf-model", "#pinguin")
-                break;
+            switch ("5") {
 
-            case "2":
-                model.setAttribute("gltf-model", "#pinguin_phoed")
-                break;
+                case "1":
+                    model.setAttribute("gltf-model", "#pinguin")
+                    break;
 
-            case "3":
-                model.setAttribute("gltf-model", "#pinguin_bhoed")
-                break;
+                case "2":
+                    model.setAttribute("gltf-model", "#pinguin_phoed")
+                    break;
 
-            case "4":
-                model.setAttribute("gltf-model", "#pinguin_stropdas")
-                break;
+                case "3":
+                    model.setAttribute("gltf-model", "#pinguin_bhoed")
+                    break;
 
-            case "5":
-                model.setAttribute("gltf-model", "#pinguin_phoed_stropdas")
-                break;
+                case "4":
+                    model.setAttribute("gltf-model", "#pinguin_stropdas")
+                    break;
 
-            case "6":
-                model.setAttribute("gltf-model", "#pinguin_bhoed_stropdas")
-                break;
+                case "5":
+                    model.setAttribute("gltf-model", "#pinguin_phoed_stropdas")
+                    break;
 
+                case "6":
+                    model.setAttribute("gltf-model", "#pinguin_bhoed_stropdas")
+                    break;
+
+            }
+
+            scene.appendChild(model);
+
+            console.log(receivedPData);
+        } else {
+            console.log("geen pinguin");
         }
-
-        scene.appendChild(model);
     }
-    console.log(receivedPData);
 
-    giraffe();
+
     olifant();
-    pinguin();
+
     console.log(sessionStorage.getItem('giraffeOutfit'));
     console.log(sessionStorage.getItem('olifantOutfit'));
     console.log(sessionStorage.getItem('pinguinOutfit'));
@@ -178,6 +204,6 @@ window.onload = function () {
             camera.setAttribute("animation", att.value);
         });
     }
-    
+
 
 };
